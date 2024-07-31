bfaa34eeef0474999b66156440d69cdf91cb0cc0
Ashish Yadava <ashishyadava1996@gmail.com>
2024-07-31 15:07:33 +0530

    configure BrowserRouter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NotFound from './NotFound';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/Blog-Master'>
       <div className="App">
         <Navbar />
         <div className="content">
